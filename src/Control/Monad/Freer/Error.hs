98e314bb35f50e015112910d8f293dcc6bc306f6
Your Name <you@example.com>
2019-08-14 22:11:30 +0000

    add mapError,fromEither

--- src/Control/Monad/Freer/Error.hs
+++ src/Control/Monad/Freer/Error.hs
@@ -14,7 +14,9 @@
 module Control.Monad.Freer.Error
   ( Error(..)
   , throwError
+  , fromEither
   , runError
+  , mapError
   , catchError
   , handleError
   ) where
@@ -22,6 +24,7 @@ module Control.Monad.Freer.Error
 import Control.Monad.Freer (Eff, Member, send)
 import Control.Monad.Freer.Interpretation
 import qualified Control.Monad.Trans.Except as E
+import Data.Bifunctor (first)
 
 -- | Exceptions of the type @e :: *@ with no resumption.
 newtype Error e r where
@@ -31,11 +34,24 @@ newtype Error e r where
 throwError :: forall e effs a. Member (Error e) effs => e -> Eff effs a
 throwError e = send (Error e)
 
+-- | Upgrade an 'Either' into an 'Error' effect.
+fromEither :: forall e effs a.Member (Error e) effs => Either e a -> Eff effs a
+fromEither (Left e) = throwError e
+fromEither (Right a) = pure a
+
 -- | Handler for exception effects. If there are no exceptions thrown, returns
 -- 'Right'. If exceptions are thrown and not handled, returns 'Left', while
 -- interrupting the execution of any other effect handlers.
 runError :: forall e effs a. Eff (Error e ': effs) a -> Eff effs (Either e a)
 runError = shortCircuit $ \(Error e) -> E.throwE e
+
+-- | Transform one 'Error' into another. This function can be used to aggregate
+-- multiple errors into a single type.
+mapError :: forall e1 e2 r a. Member (Error e2) r
+         => (e1 -> e2) -> Eff (Error e1 ': r) a -> Eff r a
+mapError f m = do
+  e1 <- runError m
+  fromEither (first f e1)
 
 -- | A catcher for Exceptions. Handlers are allowed to rethrow exceptions.
 catchError
