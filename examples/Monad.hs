61ccaaeeed74ed854c1bc1d86546667f0fdea8f8
FongHou <you@example.com>
2021-05-10 22:47:15 -0400

    examples/Monad.hs

--- examples/Monad.hs
+++ examples/Monad.hs
@@ -25,17 +25,17 @@ runState' _ (Put s `Bind` k) = runState' s (k ())
 newtype F f a = F
   {runF :: forall r. (f r -> r) -> (a -> r) -> r}
 
-foldF :: Monad m => (forall x. f x -> m x) -> F f a -> m a
-foldF alg (F f) = f (join . alg) return
-
 instance Monad (F f) where
   return a = F $ \_ _return -> _return a
   F m >>= f = F $ \alg _return -> m alg (\a -> runF (f a) alg _return)
 
+foldF :: Monad m => (forall x. f x -> m x) -> F f a -> m a
+foldF alg (F f) = f (join . alg) return
+
 -----------------------------------------------------------------------------
--- https://terrorjack.com/posts/2018-11-28-simple-freer.html
 
 newtype Eff f a = Eff
+  --   foldF :: Monad m => (forall x. f x -> m x) -> F f a -> m a
   {runFreer :: forall m. Monad m => (forall x. f x -> m x) -> m a}
 
 instance Monad (Eff f) where
@@ -49,7 +49,7 @@ newtype Cont r a = Cont {(>>-) :: (a -> r) -> r}
 
 instance Monad (Cont r) where
   return a = Cont $ \_return -> _return a
-  Cont m >>= f = Cont $ \_return -> m (\a -> f a >>- _return)
+  m >>= f = Cont $ \_return -> m >>- (\a -> f a >>- _return)
 
 runC :: Cont r r -> r
 runC m = m >>- id
@@ -62,15 +62,15 @@ shift f = Cont (runC . f)
 
 type Pure a = forall r. Cont r a
 
-runPure :: Cont r r -> r
+runPure :: Pure a -> a
 runPure (Cont m) = m id
 
 type Except e a = forall r. Cont (Either e r) a
 
-runExcept :: Cont (Either a b) b -> Either a b
+runExcept :: Except e a -> Either e a
 runExcept (Cont m) = m Right
 
-throw :: a -> Cont (Either a b) r
+throw :: e -> Except e a
 throw e = Cont (\_k -> Left e)
 
 type State s a = forall r. Cont (s -> r) a
@@ -84,25 +84,12 @@ get = Cont (\k s -> k s s)
 put :: s -> Cont (s -> s) ()
 put s = Cont (\k _s -> k () s)
 
-type ListT m a = Cont (m ()) a
-
-runListT :: (a -> r) -> Cont r a -> r
-runListT k (Cont m) = m k
-
-choose :: Applicative m => ListT m Bool
-choose = Cont (\k -> k True *> k False)
-
-empty :: Applicative m => ListT m a
-empty = Cont (\_k -> pure ())
-
 type ContT r m a = Cont (m r) a
 
 lift :: Monad m => m a -> ContT r m a
 lift m = Cont (m >>=)
 
 type Codensity m a = forall r. Cont (m r) a
-
-type F' f a = Codensity (Free f) a
 
 -------------------------------------------------------------------------------
 instance Functor (F f) where
